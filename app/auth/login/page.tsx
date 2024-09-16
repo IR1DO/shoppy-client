82151505e1a24dd5cf0c9346bc27b47c32749af6
Kevin Hu <hellokevinhu@icloud.com>
2024-09-16 22:16:35 +0800

    Implement create user server action

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <Stack spacing={2} className='w-full max-w-xs'>
       <TextField label='Email' variant='outlined' type='email' />
-      <TextField label='Password' variant='outlined' type='email' />
+      <TextField label='Password' variant='outlined' type='password' />
       <Button variant='contained'>Login</Button>
       <Link component={NextLink} href='/auth/sign-up' className='self-center'>
         Sign Up
---
+++ app/auth/sign-up/create-user.ts
@@ -0,0 +1,18 @@
+'use server';
+
+import { API_URL } from '@/app/constants/api';
+import { getErrorMessage } from '@/app/utils/errors';
+import { redirect } from 'next/navigation';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function createUser(_prevState: any, formData: FormData) {
+  const res = await fetch(`${API_URL}/users`, {
+    method: 'POST',
+    body: formData,
+  });
+  const parsedRes = await res.json();
+  if (!res.ok) {
+    return { error: getErrorMessage(parsedRes) };
+  }
+  redirect('/');
+}
