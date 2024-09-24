9c3e1f83add2ea834da2e727eac815c9963869b0
Kevin Hu <hellokevinhu@icloud.com>
2024-09-24 12:14:04 +0800

    Add header component and implement authentication context

---
+++ app/common/constants/routes.ts
@@ -0,0 +1,17 @@
+export const unauthenticatedRoutes = [
+  {
+    title: 'Login',
+    path: '/auth/login',
+  },
+  {
+    title: 'Sign-up',
+    path: '/auth/sign-up',
+  },
+];
+
+export const routes = [
+  {
+    title: 'Home',
+    path: '/',
+  },
+];
