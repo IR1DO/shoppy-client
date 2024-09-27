7b0ce66dae0f5a99f6e284ebf75127463133afb2
Kevin Hu <hellokevinhu@icloud.com>
2024-09-27 10:41:50 +0800

    Implement re-validate queries feature

--- app/products/actions/create-product.ts
+++ app/products/actions/create-product.ts
@@ -1,7 +1,10 @@
 'use server';
 
 import { post } from '@/app/common/utils/fetch';
+import { revalidateTag } from 'next/cache';
 
 export default async function createProduct(formData: FormData) {
-  return post('products', formData);
+  const response = await post('products', formData);
+  revalidateTag('products');
+  return response;
 }
