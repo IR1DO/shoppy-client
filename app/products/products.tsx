0ad81c51ef051137fbc60978a304459eeda8d974
Kevin Hu <hellokevinhu@icloud.com>
2024-10-15 22:05:41 +0800

    Implement products websockets

--- app/products/products.tsx
+++ app/products/products.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import getProducts from './actions/get-products';
-import Product from './product';
-import { Grid2 } from '@mui/material';
+import ProductsGrid from './products-grid';
 
 const Products = async () => {
   const products = await getProducts();
 
-  return (
-    // Bad practice down: add unnecessary scroll bar
-    // <Grid2 container spacing={3} sx={{ height: '85vh', overflow: 'scroll' }}>
-    <Grid2 container spacing={3}>
-      {products.map((product) => (
-        <Grid2 key={product.id} size={{ xs: 12, sm: 6, lg: 4 }}>
-          <Product product={product} />
-        </Grid2>
-      ))}
-    </Grid2>
-  );
+  return <ProductsGrid products={products} />;
 };
 
 export default Products;
